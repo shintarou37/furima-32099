b3d47fee4e80529a01e09d9ac258f8ae6f0ee995
shintarou37 <m.sintarou37@gmail.com>
2020-11-27 22:41:02 +0900

    with_options付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '6文字以上の半角英数字で記入してください' }
-  validates :birthday, presence: true
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  with_options presence: true do
+   validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   validates :last_name_kana,format: { with: /\A[ァ-ヶー－]+\z/ }
+   validates :first_name_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: '6文字以上の半角英数字で記入してください' }
+   validates :birthday
+   validates :name
+   validates :email
+   validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
