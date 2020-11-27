3bc9d1c21f8978c5f9b8702f05f1af60541b9dfb
shintarou37 <m.sintarou37@gmail.com>
2020-11-27 21:17:06 +0900

    Rubocop実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
 
   def configre_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :first_name, :last_name_kana,:first_name_kana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :last_name, presence: true,format:{ with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_name, presence: true,format:{ with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :last_name_kana, presence: true,format:{ with: /\A[ァ-ヶー－]+\z/}
-         validates :first_name_kana, presence: true,format:{ with: /\A[ァ-ヶー－]+\z/,message: "6文字以上の半角英数字で記入してください"}
-         validates :birthday, presence: true
-         validates :name, presence: true
-         validates :email, presence: true
-         validates :password, presence: true, length: { minimum: 6},format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '6文字以上の半角英数字で記入してください' }
+  validates :birthday, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
