5a28767af09b9dd892d8cfcc814319d38662a90c
shintarou37 <m.sintarou37@gmail.com>
2020-11-27 19:34:03 +0900

    devise編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
          validates :last_name, presence: true,format:{ with: /\A[ぁ-んァ-ン一-龥]/}
          validates :first_name, presence: true,format:{ with: /\A[ぁ-んァ-ン一-龥]/}
          validates :last_name_kana, presence: true,format:{ with: /\A[ァ-ヶー－]+\z/}
-         validates :first_name_kana, presence: true,format:{ with: /\A[ァ-ヶー－]+\z/}
+         validates :first_name_kana, presence: true,format:{ with: /\A[ァ-ヶー－]+\z/,message: "6文字以上の半角英数字で記入してください"}
          validates :birthday, presence: true
          validates :name, presence: true
          validates :email, presence: true
-
+         validates :password, presence: true, length: { minimum: 6},format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
