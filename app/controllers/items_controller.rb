43178dddb393762f419408c2a7fc1fd283df118e
shintarou37 <m.sintarou37@gmail.com>
2020-12-01 18:39:01 +0900

    商品編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only:[:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -22,9 +23,29 @@ class ItemsController < ApplicationController
     @items = Item.find(params[:id])
   end
 
+  def edit
+    @items = Item.find(params[:id])
+  end
+
+  def update
+    @items = Item.find(params[:id])
+    if  @items.update(item_params)  
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_fee_id, :day_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @items = Item.find(params[:id])
+    if current_user.id != @items.user_id
+      redirect_to root_path
+    end
   end
 end
