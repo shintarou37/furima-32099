9f1ef46f3864e88ff2d312c3ec190c7b33ca702a
shintarou37 <m.sintarou37@gmail.com>
2020-12-02 12:13:06 +0900

    商品一覧表示機能実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,9 +19,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @items = Item.find(params[:id])
-  # end
+  def show
+    @items = Item.find(params[:id])
+  end
 
   # def edit
   #   @items = Item.find(params[:id])
