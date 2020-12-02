c0e9cbb0312b52069df18d02d54da2ed7a1986bd
shintarou37 <m.sintarou37@gmail.com>
2020-12-02 13:36:10 +0900

    編集機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :move_to_index, only: [:edit, :update, :destory]
+  before_action :move_to_index, only: [:edit, :update, :destory]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,18 +23,18 @@ class ItemsController < ApplicationController
     @items = Item.find(params[:id])
   end
 
-  # def edit
-  #   @items = Item.find(params[:id])
-  # end
+  def edit
+    @items = Item.find(params[:id])
+  end
 
-  # def update
-  #   @items = Item.find(params[:id])
-  #   if @items.update(item_params)
-  #     redirect_to action: :show
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @items = Item.find(params[:id])
+    if @items.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   @items = Item.find(params[:id])
@@ -48,8 +48,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_fee_id, :day_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  # def move_to_index
-  #   @items = Item.find(params[:id])
-  #   redirect_to root_path if current_user.id != @items.user_id
-  # end
+  def move_to_index
+    @items = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @items.user_id
+  end
 end
