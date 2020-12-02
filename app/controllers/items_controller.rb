e1d2b03980e0b163deaebe0b3b60990036c4adaf
shintarou37 <m.sintarou37@gmail.com>
2020-12-02 16:45:36 +0900

    コントローラーリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item, only: [:edit, :update,:show,:destroy]
   before_action :move_to_index, only: [:edit, :update, :destory]
-  before_action :items, only: [:edit, :update,:show]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -27,19 +27,17 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @items.update(item_params)
+    if @item.update(item_params)
       redirect_to action: :show
     else
       render :edit
     end
   end
-  # < 商品削除機能>
+
   def destroy
-    @items = Item.find(params[:id])
-    @items.destroy
+    @item.destroy
     redirect_to root_path
   end
-  # < 商品削除機能>
 
   private
 
@@ -48,11 +46,12 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @items = Item.find(params[:id])
-    redirect_to root_path if current_user.id != @items.user_id
+     if current_user.id != @item.user_id
+      redirect_to root_path
+     end
   end
 
-  def items
-    @items = Item.find(params[:id])
+  def item
+    @item = Item.find(params[:id])
   end
 end
