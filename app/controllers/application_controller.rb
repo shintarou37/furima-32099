994c2d13ed7d677dcb274ede8dd231820d18ce6c
shintarou37 <m.sintarou37@gmail.com>
2020-12-05 11:52:40 +0900

    ビフォーアクション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configre_permitted_parameters, if: :devise_controller?
 
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
